d9aa919efa9ead87e7bf2e18ea3e8b25365d2de7
ParshantGod <113850458+ParshantGod@users.noreply.github.com>
2022-09-18 22:56:41 +0530

    Create fruits count in dictionary.py

---
+++ fruits count in dictionary.py
@@ -0,0 +1,17 @@
+allGuests = {'Alice': {'apples': 5, 'pretzels': 12},
+             'Bob': {'ham sandwiches': 3, 'apples': 2},
+             'Carol': {'cups': 3, 'apple pies': 1}}
+
+def totalBrought(guests, fruits):
+    number = 0
+    for k,v in guests.items():
+        number = number + v.get(fruits,0)
+    return number
+    
+print('Number of things being brought:')
+print(' - Apples         ' + str(totalBrought(allGuests, 'apples')))
+print(' - Cups           ' + str(totalBrought(allGuests, 'cups')))
+print(' - Cakes          ' + str(totalBrought(allGuests, 'cakes')))
+print(' - Ham Sandwiches ' + str(totalBrought(allGuests, 'ham sandwiches')))
+print(' - Apple Pies     ' + str(totalBrought(allGuests, 'apple pies')))
+        
